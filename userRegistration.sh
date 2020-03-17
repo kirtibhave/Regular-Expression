e64bd9a669ef2e9b3ef638ba497bb7e7c2a749ce
kirtibhave <kirtibhave1000@gmail.com>
2020-03-17 14:07:22 +0530

    ADDED:Function to check valid email

--- userRegistration.sh
+++ userRegistration.sh
@@ -2,10 +2,16 @@
 echo "**********Welcome to User Registration Problem**********"
 shopt -s extglob
 
+#VARIABLE
 firstName=$1
 lastName=$2
-nameRule="(^[A-Z]{1}[a-z]{2,}*)$"
+email=$3
 
+#CONSTANTS
+NAMERULE="(^[A-Z]{1}[a-z]{2,}*)$"
+emailRule="^([a-zA-Z0-9]{3,}[._+-]{1}[a-zA-Z0-9]{3,})[@]{1}[a-zA-Z0-9]*[.]{1}[a-z]{2,3}|[.]{1}[a-z]{2,3}$"
+
+#Function is used to check user's firstname and lastname 
 function checkNameRule(){
 if [[ $firstName =~ $nameRule && $lastName =~ $nameRule ]]
 then
@@ -17,6 +23,16 @@ else
 fi
 }
 
-checkNameRule
+#Function is used to check user's email address
+function checkEmail(){
+if [[ $email =~ $EMAILRULE ]]
+then
+	echo "vaild email"
+else
+	echo "invalid email"
+fi
+}
 
+checkNameRule
+checkEmail
 
