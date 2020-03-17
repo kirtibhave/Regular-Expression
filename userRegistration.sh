7d819684898bd59a8e7245357a7596cfad5c8cc1
kirtibhave <kirtibhave1000@gmail.com>
2020-03-17 15:18:12 +0530

    ADDED:Function to follow pre-defined password rules

--- userRegistration.sh
+++ userRegistration.sh
@@ -11,10 +11,12 @@ email=$3
 NAMERULE="(^[A-Z]{1}[a-z]{2,}*)$"
 EMAILRULE="^([a-zA-Z0-9]{3,}[._+-]{1}[a-zA-Z0-9]{3,})[@]{1}[a-zA-Z0-9]*[.]{1}[a-z]{2,3}|[.]{1}[a-z]{2,3}$"
 MOBILERULE="^([0-9]{2}\s{1}[0-9]{10})$"
+PASSWORDRULE="^([a-zA-Z]{8,})$"
 
 read -p "Enter mobile number" mobileNumber
+read -p "Enter password " password
 
-#Function is used to check user's firstname and lastname 
+#Function is used to check user's firstname and lastname
 function checkNameRule(){
 if [[ $firstName =~ $nameRule && $lastName =~ $nameRule ]]
 then
@@ -46,7 +48,16 @@ else
 fi
 }
 
+function checkPassword(){
+if [[ $password =~ $PASSWORDRULE ]]
+then
+	echo "valid password rule"
+else
+	echo "invalid password rule"
+fi
+}
 
 checkNameRule
 checkEmail
 checkMobileNumber
+checkPassword
