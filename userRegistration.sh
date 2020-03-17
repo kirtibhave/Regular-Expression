91d4f55ddaf8170db03fc6071a018df48f16ade0
kirtibhave <kirtibhave1000@gmail.com>
2020-03-17 19:21:09 +0530

    ADDED:Function to follow upper case rule

--- userRegistration.sh
+++ userRegistration.sh
@@ -5,14 +5,14 @@ shopt -s extglob
 #VARIABLE
 firstName=$1
 lastName=$2
-email=$3
 
 #CONSTANTS
 NAMERULE="(^[A-Z]{1}[a-z]{2,}*)$"
 EMAILRULE="^([a-zA-Z0-9]{3,}[._+-]{1}[a-zA-Z0-9]{3,})[@]{1}[a-zA-Z0-9]*[.]{1}[a-z]{2,3}|[.]{1}[a-z]{2,3}$"
 MOBILERULE="^([0-9]{2}\s{1}[0-9]{10})$"
-PASSWORDRULE="^([a-zA-Z]{8,})$"
+PASSWORDRULE="^([A-Z]{1,}|[a-zA-Z]{8,})$"
 
+read -p "Enter email address" email
 read -p "Enter mobile number" mobileNumber
 read -p "Enter password " password
 
